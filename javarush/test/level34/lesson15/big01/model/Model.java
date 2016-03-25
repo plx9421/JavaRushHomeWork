56b5d3d6eb87fe1dbe8939874fbc396f32ef6bab
Anatoliy Ivanov <plx9421@gmail.com>
2016-03-25 18:09:33 +0200

    level34.lesson15.big01.task 11

--- javarush/test/level34/lesson15/big01/model/Model.java
+++ javarush/test/level34/lesson15/big01/model/Model.java
@@ -2,16 +2,39 @@ package com.javarush.test.level34.lesson15.big01.model;
 
 import com.javarush.test.level34.lesson15.big01.controller.EventListener;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by Alexey on 23.03.2016.
  */
 public class Model {
     private EventListener eventListener;
+    private GameObjects gameObjects;
+    private int currentLevel = 1;
+    LevelLoader levelLoader = new LevelLoader(Paths.get("\\com\\javarush\\test\\level34\\lesson15\\big01\\res\\levels.txt"));
+
 
     public static final int FIELD_SELL_SIZE = 20;
 
-    public void setEventListener(EventListener eventListener){
+    public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
     }
 
+    public GameObjects getGameObjects(){
+        return gameObjects;
+    }
+
+    public void restartLevel(int level){
+        gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart(){
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel(){
+        currentLevel++;
+        restart();
+    }
 }
