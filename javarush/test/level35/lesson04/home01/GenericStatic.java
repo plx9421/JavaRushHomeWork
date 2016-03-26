dcbe83ca126a0a930bc7028a2ef77b641107648f
Anatoliy Ivanov <plx9421@gmail.com>
2016-03-26 17:17:25 +0200

    level35.lesson04.home01

--- javarush/test/level35/lesson04/home01/GenericStatic.java
+++ javarush/test/level35/lesson04/home01/GenericStatic.java
@@ -1,6 +1,6 @@
 package com.javarush.test.level35.lesson04.home01;
 
-public class GenericStatic {
+public class GenericStatic<T extends Number> {
     public static Object someStaticMethod(Object genericObject) {
         System.out.println(genericObject);
         return genericObject;
