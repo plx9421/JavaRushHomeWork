8f9797aa4e1cd4d0915830b82d2edfb8b964985e
Anatoliy Ivanov <plx9421@gmail.com>
2016-03-26 15:40:32 +0200

    level34.lesson15.big01.task 15

--- javarush/test/level34/lesson15/big01/model/Model.java
+++ javarush/test/level34/lesson15/big01/model/Model.java
@@ -12,7 +12,7 @@ public class Model {
     private EventListener eventListener;
     private GameObjects gameObjects;
     private int currentLevel = 1;
-    LevelLoader levelLoader = new LevelLoader(Paths.get("\\com\\javarush\\test\\level34\\lesson15\\big01\\res\\levels.txt"));
+    LevelLoader levelLoader = new LevelLoader(Paths.get("C:\\Users\\Alexey\\OneDrive\\Documents\\workspace.java\\JavaRushHomeWork\\src\\com\\javarush\\test\\level34\\lesson15\\big01\\res\\levels.txt"));
 
 
     public static final int FIELD_SELL_SIZE = 20;
@@ -21,25 +21,95 @@ public class Model {
         this.eventListener = eventListener;
     }
 
-    public GameObjects getGameObjects(){
+    public GameObjects getGameObjects() {
         return gameObjects;
     }
 
-    public void restartLevel(int level){
+    public void restartLevel(int level) {
         gameObjects = levelLoader.getLevel(level);
     }
 
     //TODO over ingeneering???
-    public void restart(){
+    public void restart() {
         restartLevel(currentLevel);
     }
 
-    public void startNextLevel(){
+    public void startNextLevel() {
         currentLevel++;
         restart();
     }
 
     public void move(Direction direction) {
+        Player player = gameObjects.getPlayer();
 
+        if (checkWallCollision(player, direction)) return;
+        if (checkBoxCollision(direction)) return;
+
+        moving(player, direction);
+        checkCompletion();
     }
+
+//    метод проверяет столкновение со стеной. Он должен вернуть true, если при движении
+//    объекта gameObject в направлении direction произойдет столкновение со стеной,
+//    иначе false. Для определения столкновения используй метод isCollision() у игрового
+//    объекта.
+
+    public boolean checkWallCollision(CollisionObject gameObject, Direction direction) {
+        for (GameObject g : gameObjects.getWalls()) {
+            if (gameObject.isCollision(g, direction)) return true;
+        }
+        return false;
+    }
+
+    public boolean checkBoxCollision(Direction direction) {
+        Player player = gameObjects.getPlayer();
+
+        for (Box box : gameObjects.getBoxes()) {
+            if (player.isCollision(box, direction)) {
+
+                if (checkWallCollision(box, direction)) return true;
+
+                for (Box box1 : gameObjects.getBoxes()) {
+                    if (box.equals(box1)) continue;
+                    if (box.isCollision(box1, direction)) return true;
+                }
+
+                moving(box, direction);
+            }
+        }
+
+        return false;
+    }
+
+    private void moving(Movable movable, Direction direction) {
+        switch (direction) {
+            case UP:
+                movable.move(0, -FIELD_SELL_SIZE);
+                break;
+            case DOWN:
+                movable.move(0, FIELD_SELL_SIZE);
+                break;
+            case LEFT:
+                movable.move(-FIELD_SELL_SIZE, 0);
+                break;
+            case RIGHT:
+                movable.move(FIELD_SELL_SIZE, 0);
+                break;
+        }
+    }
+
+    public void checkCompletion() {
+        for (Home home : gameObjects.getHomes()) {
+            boolean isHomeEmpty = true;
+            for (Box box : gameObjects.getBoxes()) {
+                if (box.getX() == home.getX() && box.getY() == home.getY()) {
+                    isHomeEmpty = false;
+                    break;
+                }
+            }
+            if (isHomeEmpty) return;
+        }
+        eventListener.levelCompleted(currentLevel);
+    }
+
 }
