300bf9921197af74c51e583cea3ef0eb2b04bc7c
Anatoliy Ivanov <plx9421@gmail.com>
2016-03-27 12:02:06 +0300

    level35.lesson08.task01.Finish

--- javarush/test/level35/lesson08/bonus01/ConvertableUtil.java
+++ javarush/test/level35/lesson08/bonus01/ConvertableUtil.java
@@ -9,8 +9,8 @@ public class ConvertableUtil {
     public static <N extends Convertable> Map convert(List<N> list) {
         Map result = new HashMap();
 
-        for(N l : list)
-            result.put(l.getKey(), l);
+        for (N n : list)
+            result.put(n.getKey(), n);
 
         return result;
     }
