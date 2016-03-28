cf3dc005e7aeec46006f0df2da0376c2a43451d8
Anatoliy Ivanov <plx9421@gmail.com>
2016-03-28 17:32:23 +0300

    level36.lesson04.big01 Задание 4

--- javarush/test/level36/lesson04/big01/Solution.java
+++ javarush/test/level36/lesson04/big01/Solution.java
@@ -2,7 +2,7 @@ package com.javarush.test.level36.lesson04.big01;
 
 import com.javarush.test.level36.lesson04.big01.controller.Controller;
 import com.javarush.test.level36.lesson04.big01.model.FakeModel;
-//import com.javarush.test.level36.lesson04.big01.model.MainModel;
+import com.javarush.test.level36.lesson04.big01.model.MainModel;
 import com.javarush.test.level36.lesson04.big01.model.Model;
 //import com.javarush.test.level36.lesson04.big01.view.EditUserView;
 import com.javarush.test.level36.lesson04.big01.view.UsersView;
@@ -10,7 +10,7 @@ import com.javarush.test.level36.lesson04.big01.view.UsersView;
 public class Solution {
     public static void main(String[] args)
     {
-        Model model = new FakeModel();
+        Model model = new MainModel();
         UsersView usersView = new UsersView();
         Controller controller = new Controller();
 
---
+++ javarush/test/level36/lesson04/big01/model/MainModel.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level36.lesson04.big01.model;
+
+import com.javarush.test.level36.lesson04.big01.model.service.UserService;
+import com.javarush.test.level36.lesson04.big01.model.service.UserServiceImpl;
+
+/**
+ * Created by Alexey on 28.03.2016.
+ */
+public class MainModel implements Model{
+    private UserService userService = new UserServiceImpl();
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(userService.getUsersBetweenLevels(1, 100));
+    }
+}
