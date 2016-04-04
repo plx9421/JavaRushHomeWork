b68adab6d1d4f11b65b518a0454f2dfeacc43834
Anatoliy Ivanov <plx9421@gmail.com>
2016-04-04 10:00:41 +0300

    level36.lesson10.bonus01 - finish

--- javarush/test/level36/lesson10/bonus01/Solution.java
+++ javarush/test/level36/lesson10/bonus01/Solution.java
@@ -1,5 +1,11 @@
 package com.javarush.test.level36.lesson10.bonus01;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +21,7 @@ import java.util.List;
 public class Solution {
     private List<Class> hiddenClasses = new ArrayList<>();
     private String packageName;
+
     public Solution(String packageName) {
         this.packageName = packageName;
     }
@@ -28,9 +35,80 @@ public class Solution {
     }
 
     public void scanFileSystem() throws ClassNotFoundException {
+        File dir = new File(packageName);
+        String[] classFiles = dir.list();
+        for (String file : classFiles) {
+            final String finalPath = dir.getAbsolutePath() + File.separator;
+            ClassLoader loader = new ClassLoader() {
+                @Override
+                protected Class<?> findClass(String className) throws ClassNotFoundException {
+                    byte[] temp = getBytesFromFile(finalPath + className + ".class");
+                    return defineClass(null, temp, 0, temp.length);
+                }
+
+                private byte[] getBytesFromFile(String fileName) {
+                    File file = new File(fileName);
+                    FileInputStream fis = null;
+                    try {
+                        fis = new FileInputStream(file);
+                    } catch (Exception e) {
+                        return null;
+                    }
+                    BufferedInputStream bis = new BufferedInputStream(fis);
+                    int size = (int) file.length();
+                    byte[] b = new byte[size];
+                    int rb = 0;
+                    int chunk = 0;
+                    try {
+                        while (((int) size - rb) > 0) {
+                            chunk = bis.read(b, rb, (int) size - rb);
+                            if (chunk == -1) {
+                                break;
+                            }
+                            rb += chunk;
+                        }
+                    } catch (IOException e) {
+                        try {
+                            throw new ClassNotFoundException();
+                        } catch (ClassNotFoundException e1) {
+                            e1.printStackTrace();
+                        }
+                        return null;
+                    }
+                    return b;
+                }
+            };
+
+            Class clazz = loader.loadClass(file.substring(0, file.lastIndexOf(".")));
+            if (HiddenClass.class.isAssignableFrom(clazz)) {
+                hiddenClasses.add(clazz);
+            }
+        }
     }
 
     public HiddenClass getHiddenClassObjectByKey(String key) {
+        for(Class clazz: hiddenClasses){
+            if(clazz.getSimpleName().toLowerCase().startsWith(key.toLowerCase())){
+                try {
+                    Constructor[] constructors = clazz.getDeclaredConstructors();
+                    for(Constructor constructor: constructors){
+                        if(constructor.getParameterTypes().length==0){
+                            constructor.setAccessible(true);
+                            return (HiddenClass) constructor.newInstance(null);
+                        }
+                    }
+                }
+                catch (InstantiationException e) {
+                    e.printStackTrace();
+                }
+                catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return null;
     }
 }
