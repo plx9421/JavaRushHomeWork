af389f6eed7084443263a386cdaebe6cb27947e1
Anatoliy Ivanov <plx9421@gmail.com>
2016-04-05 12:54:43 +0300

    level27.lesson15.big01.Task 2

--- javarush/test/level27/lesson15/big01/Tablet.java
+++ javarush/test/level27/lesson15/big01/Tablet.java
@@ -1,16 +1,38 @@
 package com.javarush.test.level27.lesson15.big01;
 
+
+import com.javarush.test.level27.lesson15.big01.kitchen.Order;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Created by Alexey on 05.04.2016.
  */
 public class Tablet {
     private final int number;
+    private static Logger logger = Logger.getLogger(Tablet.class.getName());
 
     public Tablet(int number) {
         this.number = number;
     }
 
-    public void createOrder(){
+    public void createOrder() {
+        Order order = null;
+        try {
+            order = new Order(this);
+            if (order != null) {
+                ConsoleHelper.writeMessage(order.toString());
+            }
 
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Console is unavailable.");
+        }
+
+    }
+
+    public int getNumber() {
+        return number;
     }
 }
--- javarush/test/level27/lesson15/big01/kitchen/Order.java
+++ javarush/test/level27/lesson15/big01/kitchen/Order.java
@@ -1,7 +1,27 @@
 package com.javarush.test.level27.lesson15.big01.kitchen;
 
+import com.javarush.test.level27.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level27.lesson15.big01.Tablet;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by Alexey on 05.04.2016.
  */
 public class Order {
+    private List<Dish> dishes;
+    private Tablet table;
+
+    public Order(Tablet table) throws IOException {
+        this.table = table;
+        this.dishes = ConsoleHelper.getAllDishesForOrder();
+    }
+
+    @Override
+    public String toString() {
+        if (dishes == null || dishes.isEmpty()) return "";
+        return "Your order: " + dishes + " of Tablet{number=" + table.getNumber() + "}";
+    }
 }
+
