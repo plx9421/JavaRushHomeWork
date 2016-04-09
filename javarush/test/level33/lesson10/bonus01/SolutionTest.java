92500684d29ee89dab43c986c6fccd70bfb0e8c4
Anatoliy Ivanov <plx9421@gmail.com>
2016-04-09 18:31:36 +0300

    level33.lesson10.bonus01 - Стеклофон

--- javarush/test/level33/lesson10/bonus01/SolutionTest.java
+++ javarush/test/level33/lesson10/bonus01/SolutionTest.java
@@ -1,20 +1,25 @@
 package com.javarush.test.level33.lesson10.bonus01;
 
-import com.javarush.test.level33.lesson10.home01.Shop;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by Alexey on 16.03.2016.
+ * Created by Alexey on 09.04.2016.
  */
 public class SolutionTest {
 
     @Test
     public void testToXmlWithComment() throws Exception {
-//        System.out.println(new Solution().toXmlWithComment(new Shop(), "count", "test a comment"  ));
 
-        System.out.println(new Solution().toXmlWithComment(new AnExample(), "needCDATA", "it's a comment - <needCDATA>"));
-
+        String actual = Solution.toXmlWithComment(new AnExample(), "needCDATA", "it's a comment - <needCDATA>");
+        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\r\n" +
+                "<anExample>\r\n" +
+                "    <!--it's a comment - <needCDATA>-->\r\n" +
+                "    <needCDATA><![CDATA[need CDATA because of < and >]]></needCDATA>\r\n" +
+                "    <!--it's a comment - <needCDATA>-->\r\n" +
+                "    <needCDATA/>\r\n" +
+                "</anExample>\r\n";
+        assertEquals(expected, actual);
     }
 }
