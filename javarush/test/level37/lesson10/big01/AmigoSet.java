e435ce0d175b1d9dda1db5bf9e5f0e9b8256e424
Anatoliy Ivanov <plx9421@gmail.com>
2016-04-11 12:16:13 +0300

    level37.lesson10.big01.task2

--- javarush/test/level37/lesson10/big01/AmigoSet.java
+++ javarush/test/level37/lesson10/big01/AmigoSet.java
@@ -1,15 +1,29 @@
 package com.javarush.test.level37.lesson10.big01;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Alexey on 11.04.2016.
  */
-public class AmigoSet<E> extends AbstractSet implements Serializable, Cloneable, Set{
-    private final Object PRESENT = new Object();
+public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
+    private static final Object PRESENT = new Object();
+    private HashMap<E, Object> map;
+
+    public AmigoSet() {
+        map = new HashMap<>();
+    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        int capacity = ((16 > collection.size() / .75f) ? 16 : (int) (collection.size() / .75f));
+        map = new HashMap<>(capacity);
+        this.addAll(collection);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return map.put(e, PRESENT) == null;
+    }
 
     @Override
     public Iterator iterator() {
