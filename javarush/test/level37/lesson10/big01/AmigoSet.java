a36850b4575fa019b00c0885e22ee8831f1d991a
Anatoliy Ivanov <plx9421@gmail.com>
2016-04-11 10:46:14 +0300

    level37.lesson10.big01

---
+++ javarush/test/level37/lesson10/big01/AmigoSet.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level37.lesson10.big01;
+
+import java.io.Serializable;
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by Alexey on 11.04.2016.
+ */
+public class AmigoSet<E> extends AbstractSet implements Serializable, Cloneable, Set{
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
