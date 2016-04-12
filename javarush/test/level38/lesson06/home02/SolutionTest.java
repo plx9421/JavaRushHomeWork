a868587e69edb95629e2cd51dbc4e1ab92e879be
Anatoliy Ivanov <plx9421@gmail.com>
2016-04-12 17:14:55 +0300

    level38.lesson06.home02

---
+++ javarush/test/level38/lesson06/home02/SolutionTest.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level38.lesson06.home02;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Alexey on 12.04.2016.
+ */
+public class SolutionTest {
+
+    @Test
+    public void testWrite() throws Exception {
+
+        new Solution().write("  ");
+    }
+
+    @Test
+    public void testRead() throws Exception {
+
+    }
+
+    @Test
+    public void testDisconnect() throws Exception {
+
+    }
+}
