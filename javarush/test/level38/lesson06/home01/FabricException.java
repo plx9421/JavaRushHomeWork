c803ae9d42f814e824cc531c0401d4ca6033d1a7
Anatoliy Ivanov <plx9421@gmail.com>
2016-04-12 16:45:55 +0300

    level38.lesson06.home01 - оптимизировал

--- javarush/test/level38/lesson06/home01/FabricException.java
+++ javarush/test/level38/lesson06/home01/FabricException.java
@@ -5,20 +5,17 @@ package com.javarush.test.level38.lesson06.home01;
  */
 public class FabricException {
     public static Throwable getException(Enum message) {
-        String result = "";
+        String result = message.toString().charAt(0) + message.toString().substring(1).replaceAll("_", " ").toLowerCase();
+
         if (message instanceof ExceptionApplicationMessage) {
-            result = message.toString().substring(0, 1) + message.toString().substring(1).replace("_", " ").toLowerCase();
             return new Exception(result);
         }
         if (message instanceof ExceptionDBMessage) {
-            result = message.toString().substring(0, 1) + message.toString().substring(1).replace("_", " ").toLowerCase();
             return new RuntimeException(result);
         }
         if (message instanceof ExceptionUserMessage) {
-            result = message.toString().substring(0, 1) + message.toString().substring(1).replace("_", " ").toLowerCase();
             return new Error(result);
         }
-
 
         return new IllegalArgumentException();
     }
